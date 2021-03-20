529c54c1a25bf10d224e7a77bcba2824353548e0
oopanpan <oopanpan@gmail.com>
2021-03-20 16:56:13 -0700

    bootstrap imported

--- src/MainDisplay.js
+++ src/MainDisplay.js
@@ -1,22 +1,47 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import NavbarDiv from './Navbar'
 
 const MainDisplay = () => {
+
     const [data, setData] = useState(0)
+    const [states, setStates] = useState(0)
+    const [date, setDate] = useState(0)
 
     useEffect( () => {
-        vaccineAPI();
-    }) 
+        vaccineAPI()
+    },[]) 
 
     const vaccineAPI = async () =>{
         const dataUrl = 'https://data.cdc.gov/resource/saz5-9hgg.json';
-        const data = await axios(dataUrl)
-            .then(r => console.log(r.data))
+        await axios(dataUrl)
+            .then(r => {
+                sortingData(r.data)
+            })
+    }
+
+    const sortingData = (data) =>{
+        let stateArr = [];
+        let dateObj = {};
+        for (let i = 0; i < 63; i++){
+            stateArr.push(data[i].jurisdiction)
+            console.log(i);
+        }
+        for (let info in data){
+            if (!Object.keys(dateObj).includes(info.week_of_allocations)){
+                console.log(Object.keys(dateObj))
+                dateObj[info.week_of_allocations] = [info]
+            } else{
+                dateObj[info.week_of_allocations].push(info)
+            }
+        }
+        setData(dateObj)
+        setStates(stateArr)
     }
 
     return (
         <>
-            <Navbar />
+            <NavbarDiv />
         </>
     )
 }
